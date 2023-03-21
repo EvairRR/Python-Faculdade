891f2f655749b77a144afe4781a41cc4a40219a4
Evair Rodrigues <108604560+EvairRR@users.noreply.github.com>
2023-03-21 11:19:04 -0300

    Add files via upload

---
+++ condicional.py
@@ -0,0 +1,9 @@
+if codigo_compra == 5222:
+print ("Compra á vista")
+elif codigo_compra ==5333:
+    print ("Compra a prazo no boleto")
+    elif codigo_compra == 5444:
+        print ("Compra a prazo no cartão")
+        else:
+            print("Código não cadastrado")
+            
