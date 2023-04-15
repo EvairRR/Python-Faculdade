15dfe4594ff985aa9451b49560b4683f63b53c14
Evair Rodrigues <108604560+EvairRR@users.noreply.github.com>
2023-04-15 10:51:07 -0300

    Add files via upload

---
+++ break e continue.py
@@ -0,0 +1,8 @@
+# Exemplo de uso de break
+disciplina = "Linguagem de programação"
+
+for c in disciplina:
+     if c == 'a' :
+           break
+     else:
+           print (c)
---
+++ continue.py
@@ -0,0 +1,7 @@
+# Exemplo de uso de continue
+disciplina = " Linguagem de progaramção"
+for c in disciplina:
+    if c == "a":
+         continue
+    else: 
+         print(c)
