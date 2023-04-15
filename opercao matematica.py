c5f7930e515ed8184ab23cdf354d6b56c7d3841b
Evair Rodrigues <108604560+EvairRR@users.noreply.github.com>
2023-04-15 07:47:24 -0300

    Add files via upload

---
+++ opercao matematica.py
@@ -0,0 +1,13 @@
+#Qual resultado armazenado na variável operação_1: 25 ou 17?
+operacao_1 = 2 + 3 * 5
+#Qual resultado armazenado na variável operação_2: 25 ou 17?
+operacao_2 = (2 + 3) * 5 
+#Qual resultado armazenado na variável operação_3: 4 ou 1?
+operacao_3 = 4 / 2 ** 2
+#Qual resultado armazenado na variável operação_4: 4 ou 5?
+operacao_4 = 13 % 3 + 4 
+
+print (operacao_1) 
+print (operacao_2)
+print (operacao_3)
+print (operacao_4)
